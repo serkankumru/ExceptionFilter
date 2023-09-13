df4e46a010f9172a377a28ecfc7bb58f2a70f417
Serkan Kumru <serkankumru1997@gmail.com>
2023-09-13 12:30:25 +0300

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessLayer;
+using FirstMVC.Models;
+
+namespace FirstMVC.Controllers
+{
+    [MyExceptionFilter]
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            if(Session["User"] == null)
+            {
+                return View("Login",new LoginViewModel());
+            }
+            var crx = new CareerEntities();
+            var users = (Users)Session["User"];
+            Candidate c = crx.Candidate.FirstOrDefault(x=>x.UserId==users.Id);
+           
+            List<CandidateApplicant> basvurularim = crx.CandidateApplicant.Where(y => y.CandidateId == c.Id).ToList();
+            List<Applicant> aplList= crx.Applicant.ToList();
+
+            //işler
+            foreach (var item in crx.Applicant.ToList())
+            {
+                foreach (var item2 in basvurularim)
+                {
+                    if (item.JobId == item2.Applicant.JobId)
+                    {
+                        aplList.Remove(item);
+                    }
+                }
+            }
+
+
+            ViewBag.BasvuruJobs = basvurularim;
+            ViewBag.BasvurulmayanJobs = aplList;
+            return View(c);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Error()
+        {
+            ViewBag.Message = "Your error page.";
+
+            return View();
+        }
+    }
+}
---
+++ MyExceptionFilter.cs
@@ -0,0 +1,32 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstMVC.Extensions;
+
+namespace FirstMVC.Models
+{
+    public class MyExceptionFilter :FilterAttribute,IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            var ctx = new CareerEntities();
+            ErrorLog lg = new ErrorLog();
+            if (filterContext.RequestContext.HttpContext.Session["User"] != null)
+                lg.UserId = ((Users)filterContext.RequestContext.HttpContext.Session["User"]).Id;
+            lg.error = filterContext.Exception.ToString().ControlledSub(1000, "");
+            lg.rawId = filterContext.RequestContext.HttpContext.Request.RawUrl;
+            lg.createDate = DateTime.Now;
+
+            ctx.ErrorLog.Add(lg);
+            ctx.SaveChanges();
+          
+            
+            
+            filterContext.RequestContext.HttpContext.Response.Redirect("/Home/Error");
+            filterContext.RequestContext.HttpContext.Response.End();
+        }
+    }
+}
