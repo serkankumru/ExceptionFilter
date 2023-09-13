0e4b0f1611278c9f7f464154da39095a4d5539ab
Serkan Kumru <serkankumru1997@gmail.com>
2023-09-13 12:46:37 +0300

    Add files via upload

---
+++ MyFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace News.Filter
+{
+    public class MyFilter:ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+        }
+        //public override void OnActionExecuted(ActionExecutedContext filterContext)
+        //{
+        //    base.OnActionExecuting(filterContext);
+        //}
+        //public override void OnActionExecuted(ActionExecutedContext filterContext)
+        //{
+        //    base.OnResultExecuted(filterContext);
+        //}
+        //public override void OnActionExecuted(ActionExecutedContext filterContext)
+        //{
+        //    base.OnResultExecuting(filterContext);
+        //}
+    }
+}
