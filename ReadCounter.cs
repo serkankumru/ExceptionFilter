267269501f2da59190b0ce4f350fa454c958efe2
Serkan Kumru <serkankumru1997@gmail.com>
2023-09-14 13:21:15 +0300

    Add files via upload

---
+++ ReadCounter.cs
@@ -0,0 +1,20 @@
+﻿using DAL;
+using DAL.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace News.Filter
+{
+    public class ReadCounter:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            int id = Convert.ToInt32(filterContext.ActionParameters["id"]);
+            var newsRepository = new NewsRepository();
+            newsRepository.ReadCount(id);
+        }
+    }
+}
