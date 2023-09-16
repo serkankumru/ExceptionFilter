ed08d1c5cffbc2f3cb9de06d555a87b4f31f6fdd
Serkan Kumru <serkankumru1997@gmail.com>
2023-09-16 11:55:37 +0300

    Add files via upload

---
+++ Filter/ExceptionFilter.cs
@@ -0,0 +1,27 @@
+﻿using DAL;
+using System;
+using System.Web.Mvc;
+
+namespace News.Filter
+{
+    public class ExceptionFilter :FilterAttribute,IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            var ctx = new NewsEntities();
+            ErrorLog lg = new ErrorLog();
+            
+            lg.Error = filterContext.Exception.ToString().Substring(150);
+            lg.RawId = filterContext.RequestContext.HttpContext.Request.RawUrl;
+            lg.CreateDate = DateTime.Now;
+
+            ctx.ErrorLog.Add(lg);
+            ctx.SaveChanges();
+          
+            
+            
+            filterContext.RequestContext.HttpContext.Response.Redirect("/Home/Error");
+            filterContext.RequestContext.HttpContext.Response.End();
+        }
+    }
+}
---
+++ Filter/ReadCounter.cs
@@ -0,0 +1,26 @@
+﻿using DAL;
+using DAL.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Haberler.Filter
+{
+    public class ReadCounter:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.ActionParameters["id"] != null)
+            {
+                int id = Extensions.Extensions.GetNewsTitleId(filterContext.ActionParameters["id"].ToString());
+                if (id != 0)
+                {
+                    var newsRepository = new NewsRepository();
+                    newsRepository.ReadCount(id);
+                }
+            }               
+        }
+    }
+}
